41f8762176e9925fa62d7a0403d335328c4022ad
Jeremy Bensen <j.xpsxpress@gmail.com>
2026-08-06 01:24:58 +0000

    File changes

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,26 @@
 const CACHE = 'visualx-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
-  const url = new URL(e.request.url);
-  if (url.origin !== self.location.origin) return;
-
   e.respondWith(
     caches.match(e.request).then((cached) => {
-      const network = fetch(e.request).then((response) => {
-        if (response && response.status === 200) {
+      return cached || fetch(e.request).then((response) => {
+        if (response && response.status === 200 && response.type === 'basic') {
           const clone = response.clone();
           caches.open(CACHE).then((c) => c.put(e.request, clone));
         }
         return response;
       }).catch(() => cached);
-      return cached || network;
     })
   );
 });
